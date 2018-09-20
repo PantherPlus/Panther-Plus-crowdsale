91cf7e8acb4f09e9fc3316e1e6eeabab9d36c3a3
PantherPlus <32623631+PantherPlus@users.noreply.github.com>
2018-09-20 22:00:03 +0300

    version comp 0.4.24

--- contracts/BasicToken.sol
+++ contracts/BasicToken.sol
@@ -1,103 +1,50 @@
-pragma solidity ^0.4.11;
-import './Utils.sol';
-import './interfaces/IERC20Token.sol';
+pragma solidity ^0.4.24;
+
+
+import "./ERC20Basic.sol";
+import "./SafeMath.sol";
+
 
 /**
-    ERC20 Standard Token implementation
-*/
-contract ERC20Token is IERC20Token, Utils {
-    string public standard = "Token 0.1";
-    string public name = "";
-    string public symbol = "";
-    uint8 public decimals = 0;
-    uint256 public totalSupply = 0;
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  mapping(address => uint256) internal balances;
 
-    /**
-        @dev constructor
+  uint256 internal totalSupply_;
 
-        @param _name        token name
-        @param _symbol      token symbol
-        @param _decimals    decimal points, for display purposes
-    */
-    function ERC20Token(string _name, string _symbol, uint8 _decimals) {
-        require(bytes(_name).length > 0 && bytes(_symbol).length > 0); // validate input
+  /**
+  * @dev Total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return totalSupply_;
+  }
 
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-    }
+  /**
+  * @dev Transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_value <= balances[msg.sender]);
+    require(_to != address(0));
 
-    /**
-        @dev send coins
-        throws on any error rather then return a false flag to minimize user errors
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
 
-        @param _to      target address
-        @param _value   transfer amount
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) public view returns (uint256) {
+    return balances[_owner];
+  }
 
-        @return true if the transfer was successful, false if it wasn't
-    */
-    function transfer(address _to, uint256 _value)
-        public
-        validAddress(_to)
-        returns (bool success)
-    {
-        balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], _value);
-        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
-        Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    /**
-        @dev an account/contract attempts to get the coins
-        throws on any error rather then return a false flag to minimize user errors
-
-        @param _from    source address
-        @param _to      target address
-        @param _value   transfer amount
-
-        @return true if the transfer was successful, false if it wasn't
-    */
-    function transferFrom(address _from, address _to, uint256 _value)
-        public
-        validAddress(_from)
-        validAddress(_to)
-        returns (bool success)
-    {
-        allowance[_from][msg.sender] = safeSub(allowance[_from][msg.sender], _value);
-        balanceOf[_from] = safeSub(balanceOf[_from], _value);
-        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    /**
-        @dev allow another account/contract to spend some tokens on your behalf
-        throws on any error rather then return a false flag to minimize user errors
-
-        also, to minimize the risk of the approve/transferFrom attack vector
-        (see https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM/), approve has to be called twice
-        in 2 separate transactions - once to change the allowance to 0 and secondly to change it to the new allowance value
-
-        @param _spender approved address
-        @param _value   allowance amount
-
-        @return true if the approval was successful, false if it wasn't
-    */
-    function approve(address _spender, uint256 _value)
-        public
-        validAddress(_spender)
-        returns (bool success)
-    {
-        // if the allowance isn't 0, it can only be updated to 0 to prevent an allowance change immediately after withdrawal
-        require(_value == 0 || allowance[msg.sender][_spender] == 0);
-
-        allowance[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
 }
